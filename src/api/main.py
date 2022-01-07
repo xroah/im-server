f4174528f5e94d7687135bca28e66ef4667f844f
xroah <396069583@qq.com>
2022-01-07 17:59:48 +0800

    upate

--- src/api/main.py
+++ src/api/main.py
@@ -1,10 +1,27 @@
-from fastapi import APIRouter
+from fastapi import Request, FastAPI, responses
+import time
+import json
 from .login import main
 
-api = APIRouter(prefix="/api")
+api = FastAPI()
 api.include_router(main.router)
 
 
 @api.get("/")
 async def root():
     return "Welcome"
+
+
+@api.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    if ("authorization" not in request.headers) or (not request.headers["authorization"]):
+        data = json.dumps({"code": 401, "msg": "没有权限"})
+
+        return responses.JSONResponse(status_code=401, content=data)
+
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+
+    return response
--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from api.main import api
+import uvicorn
 
 app = FastAPI()
+app.mount("/api", api)
 
-app.include_router(api)
 
 @app.get("/")
 async def root():
     return "Hello world"
 
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8888)
