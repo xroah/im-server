2e7bff5a2c809bfe510e999944087696911429c0
xroah <396069583@qq.com>
2022-01-15 22:18:59 +0800

    login

--- src/api/routers/account/main.py
+++ src/api/routers/account/main.py
@@ -1,16 +1,36 @@
 from fastapi import APIRouter
 from sqlalchemy import select
+from pydantic import BaseModel
 from ...db.tables import Account
 from ...db.main import session
+from ...utils import md5
 
 router = APIRouter(prefix="/account")
 
 
-@router.get("/login")
-def login():
-    result = session.execute(select(Account.username, Account.password).limit(1).offset(1))
+class LoginParam(BaseModel):
+    username: str
+    password: str
 
-    return result.all()
+
+@router.post("/login")
+def login(param: LoginParam):
+    result = session.execute(
+        select(Account.username, Account.password).
+        where(
+            Account.username == param.username,
+            Account.password == md5(param.password)
+        ).
+        limit(1)
+    ).all()
+
+    if len(result) == 0:
+        return {"code": -1, "msg": "用户名或密码错误"}
+
+    return {
+        "code": 0,
+        "msg": "登录成功!"
+    }
 
 
 @router.get("/register")
