9984f9db8de674e5d26bea3255e3c2d8da344058
xroah <396069583@qq.com>
2022-01-17 16:22:53 +0800

    redis

--- src/api/db/redis.py
+++ src/api/db/redis.py
@@ -1,3 +1,16 @@
 import redis
+from typing import Any
 
 redis_conn = redis.Redis(host="localhost", port=6379, db=0)
+
+
+def set(key: str, value: Any):
+    return redis_conn.set(key, value, ex=7 * 24 * 3600)
+
+
+def expire(key: str, time: int):
+    return redis_conn.expire(key, time)
+
+
+def delete(key: str):
+    return redis_conn.delete(key)
