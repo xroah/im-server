464e26474cca4af5896d116a4278934be253862e
xroah <396069583@qq.com>
2022-05-06 11:19:10 +0800

    delete

--- src/api/codeenum.py
+++
@@ -1,9 +0,0 @@
-from enum import IntEnum
-
-
-# enum for response code
-class Code(IntEnum):
-    SUCCESS = 0
-    NO_PERM = 401  # no permission
-    UNKNOWN_ERROR = 1000  # unknown error
-    COMMON_ERROR = 1001
--- src/api/db/mongo.py
+++
@@ -1,6 +0,0 @@
-from motor import motor_asyncio, MotorCollection
-
-client = motor_asyncio.AsyncIOMotorClient()
-db = client["account_book"]
-users_coll = db["users"]
-
